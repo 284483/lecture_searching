b5d5db9cbb08bc863d94f76755089a4b48bb3cb6
284483 <284483@vutbr.cz>
2026-04-07 13:18:42 +0200

    ok

--- searching.py
+++ searching.py
@@ -1,26 +1,16 @@
 import os
-
+import json
 # get current working directory path
 cwd_path = os.getcwd()
 
 
 def read_data(file_name, field):
-    """
-    Reads json file and returns sequential data.
-    :param file_name: (str), name of json file
-    :param field: (str), field of a dict to return
-    :return: (list, string),
-    """
-    try:
-        with open(file_name, 'r') as file:
-            data = json.load(file)
-        if field not in data:
-            return None
-
-        return data[field]
-
-    except:
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+    if field not in data:
         return None
+    return data[field]
+
 
 def main():
 
